8b035f7d3e9573a309313fb527148d79ec2fe214
c <hackerjieson@gmail.com>
2020-05-04 21:05:46 +0800

    modify

--- cron.go
+++ cron.go
@@ -2,32 +2,27 @@ package gcron
 
 import (
 	"encoding/json"
-	"log"
-	"sort"
-	"strconv"
+	"errors"
+	"fmt"
 	"strings"
 	"time"
 )
 
-//linux下crontab的golang实现,不支持单个标志位的复合写法 比如 2-8/2, 请使用2,4,6,8代替
-// 分、时、日、月、周
-// * 取值范围内的所有数字
-// / 每过多少个数字
-// - 从X到Z
-// ，散列数字
+//linux下crontab的golang实现
 
 type Task struct {
-	Second    Rule       //秒 规则
-	Minute    Rule       //分 规则
-	Hour      Rule       //时 规则
-	Day       Rule       //日 规则
-	Month     Rule       //月 规则
-	Week      Rule       //周 规则
-	JoinAt    time.Time  //任务添加时间
-	LastRunAt *time.Time //上次执行时间
-	NextRunAt *time.Time //执行时间
-	ExecTimes int        //执行次数
-	TaskData  TaskData   //任务数据
+	Minute         []int      //分 规则
+	Hour           []int      //时 规则
+	Dom            []int      //日 规则
+	Month          []int      //月 规则
+	Dow            []int      //周 规则
+	JoinAt         time.Time  //任务添加时间
+	LastRunAt      *time.Time //上次执行时间
+	NextRunAt      *time.Time //执行时间
+	ExecTimes      int        //执行次数
+	TaskData       TaskData   //任务数据
+	LocationOffset int        //当前时区与UTC的偏移秒数
+	LocationName   string     //当前时区名
 }
 
 //具体任务内容
@@ -41,85 +36,61 @@ type TaskData struct {
 	Desc            string                 //任务描述
 }
 
-//任务规则
-type Rule struct {
-	RunType RunType
-	Value   []int
-}
-type RunType int //周期类型,不直接针对任务,而是任务下面的时分秒
-const (
-	Every    RunType = iota //范围内所有数字
-	Assign                  //指定具体数字
-	Interval                //每间隔多久执行一次
-)
-
 //任务管理
 type CronManager struct {
-	Tasks    []*Task
-	Location *time.Location
+	Tasks []*Task
 }
 
 //创建任务管理器
-func NewCronManager(location *time.Location) *CronManager {
+func NewCronManager() *CronManager {
 	return &CronManager{
-		Tasks:    make([]*Task, 100),
-		Location: location,
+		Tasks: make([]*Task, 100),
 	}
 }
 
 //添加任务
-func (cm *CronManager) Add(second, minute, hour, day, month, week, taskJson string) {
+func (cm *CronManager) Add(cronRule, taskJson string, locationOffset int) error {
+	var err error
 	taskData := TaskData{}
-	err := json.Unmarshal([]byte(taskJson), &taskData)
+	err = json.Unmarshal([]byte(taskJson), &taskData)
 	if err != nil {
-		log.Fatalln("数据格式错误")
+		return errors.New("任务格式错误")
+	}
+	ruleItems := strings.Split(cronRule, " ")
+	if len(ruleItems) != 5 {
+		return errors.New("cron规则格式错误")
 	}
 	task := &Task{
-		Second:    parse(second),
-		Minute:    parse(minute),
-		Hour:      parse(hour),
-		Day:       parse(day),
-		Month:     parse(month),
-		Week:      parse(week),
-		JoinAt:    time.Now(),
-		LastRunAt: nil,
-		NextRunAt: nil,
-		ExecTimes: 0,
-		TaskData:  taskData,
+		JoinAt:         time.Now(),
+		LastRunAt:      nil,
+		NextRunAt:      nil,
+		ExecTimes:      0,
+		TaskData:       taskData,
+		LocationOffset: locationOffset,
 	}
-	task.ComputeNextRunAt()
-	cm.Tasks = append(cm.Tasks, task)
-}
-
-//规则解析
-func parse(param string) Rule {
-	if ok := strings.ContainsRune(param, '/'); ok {
-		arr := strings.Split(param, "/")
-		value, _ := strconv.Atoi(arr[1])
-		return Rule{RunType: Interval, Value: []int{value}}
+	task.Minute, err = cronRuleParse(ruleItems[0], []int{0, 59})
+	if err != nil {
+		return errors.New("分钟" + err.Error())
 	}
-	if ok := strings.ContainsRune(param, '-'); ok {
-		arr := strings.Split(param, "-")
-		value := make([]int, 31)
-		begin, _ := strconv.Atoi(arr[0])
-		end, _ := strconv.Atoi(arr[1])
-		for i := begin; i <= end; i++ {
-			value = append(value, i)
-		}
-		sort.Ints(value)
-		return Rule{RunType: Assign, Value: value}
+	task.Hour, err = cronRuleParse(ruleItems[1], []int{0, 23})
+	if err != nil {
+		return errors.New("小时" + err.Error())
 	}
-	if ok := strings.ContainsRune(param, ','); ok {
-		arr := strings.Split(param, ",")
-		value := make([]int, 31)
-		for i := 0; i <= len(arr); i++ {
-			tmp, _ := strconv.Atoi(arr[i])
-			value = append(value, tmp)
-		}
-		sort.Ints(value)
-		return Rule{RunType: Assign, Value: value}
+	task.Dom, err = cronRuleParse(ruleItems[2], []int{0, 23})
+	if err != nil {
+		return errors.New("日(月)" + err.Error())
 	}
-	return Rule{RunType: Every, Value: nil}
+	task.Month, err = cronRuleParse(ruleItems[3], []int{0, 23})
+	if err != nil {
+		return errors.New("月" + err.Error())
+	}
+	task.Dow, err = cronRuleParse(ruleItems[4], []int{0, 23})
+	if err != nil {
+		return errors.New("周(月)" + err.Error())
+	}
+	fmt.Println(task.ComputeNextRunAt())
+	//cm.Tasks = append(cm.Tasks, task)
+	return nil
 }
 
 //任务列表
@@ -127,78 +98,59 @@ func (cm *CronManager) List() []*Task {
 	return cm.Tasks
 }
 
-//时间规则范围
-var (
-	seconds = [2]int{0, 59}
-	minutes = [2]int{0, 59}
-	hours   = [2]int{0, 23}
-	day     = [2]int{1, 31}
-	months  = [2]int{1, 12}
-	week    = [2]int{0, 6}
-)
-
-func (t *Task) ComputeNextRunAt() {
-	//second, minute, hour, day, month, week
-	//"*/10", "*/2",  "*",  "*", "*/3", "*"
-	//2020-04-27 14:16:30
-	now := time.Now()
-	nextRunAt := time.Now()
+func (t *Task) ComputeNextRunAt() string {
+	now := time.Now().In(time.FixedZone(t.LocationName, t.LocationOffset))
+	nowCopy := now
 	var (
-		month = int(now.Month())
-		//day    = now.Day()
-		//hour   = now.Hour()
-		//minute = now.Minute()
-		//second = now.Second()
-		week = int(now.Weekday())
+		month  = int(now.Month())
+		week   = int(now.Weekday())
+		day    = now.Day()
+		hour   = now.Hour()
+		minute = now.Minute()
 	)
-	//处理 Month 规则
-	switch t.Month.RunType {
-	case Assign:
-		//[1,2,3,6,8] current:4
-		//当月不再指定月份中,选择最近的一个月份
-		if !existsInArray(t.Month.Value, month) {
-			targetMonth := getMinValueInArray(t.Month.Value, month)
-			nextRunAt = nextRunAt.AddDate(0, targetMonth-month, 0)
+	//当前月份不在规则中
+	if !existsInArray(t.Month, month) {
+		nextMonth := getMinValueInArray(t.Month, month)
+		if nextMonth > month {
+			nowCopy.AddDate(0, nextMonth-month, 0)
 		} else {
-			//todo 如果当月在指定月份中,不做处理,但是需要条件回查
-		}
-	case Interval:
-		if t.LastRunAt == nil {
-			nextRunAt = nextRunAt.AddDate(0, t.Month.Value[0], 0)
-		} else {
-			lastMonthAt := int(t.LastRunAt.Month())
-			targetMonth := lastMonthAt + t.Month.Value[0] - month
-			nextRunAt = nextRunAt.AddDate(0, targetMonth, 0)
+			nowCopy.AddDate(0, nextMonth+12-month, 0)
 		}
 	}
-	//处理 Week 规则
-	switch t.Week.RunType {
-	case Assign:
-		//[3,6,0] current:1
-		//当周不再指定周中,那么选择最近的一个周
-		if !existsInArray(t.Week.Value, week) {
-			targetWeek := getMinWeekInArray(t.Week.Value, week)
-			addDay := 0
-			if targetWeek != week {
-				if targetWeek == 0 {
-					addDay = 7 - week
-				} else {
-					addDay = targetWeek - week
-				}
-			}
-			nextRunAt = nextRunAt.AddDate(0, 0, addDay)
+	//当前周(月)不在规则中
+	//todo  和日 or关系
+	if !existsInArray(t.Dow, week) {
+		nextWeek := getMinWeekInArray(t.Dow, week)
+		nowCopy.AddDate(0, 0, nextWeek-week)
+	}
+	//当前日(月)不在规则中
+	//todo  和周 or关系,, 还可能存在条件超越父级
+	if !existsInArray(t.Dom, day) {
+		nextDay := getMinValueInArray(t.Dom, day)
+		if nextDay > day {
+			nowCopy.AddDate(0, 0, nextDay-day)
 		} else {
-			//todo 如果当周在指定周中,不做处理,但是需要条件回查
-		}
-	case Interval:
-		//还没有执行过
-		if t.LastRunAt == nil {
-			nextRunAt = nextRunAt.AddDate(0, 0, t.Week.Value[0]*7)
-		} else {
-			//temp := t.LastRunAt.AddDate(0, 0, t.Week.Value[0]*7)
-			//addDay := (int(temp.Unix()) - seconds) / 86400
-			//targetWeek := lastWeekAt + t.Week.Value[0] - week
-			//nextRunAt = nextRunAt.AddDate(0, targetMonth, 0)
+			currentMonthHasDays := getCurrentMonthHasDays(t.LocationName, t.LocationOffset)
+			nowCopy.AddDate(0, 0, nextDay+(currentMonthHasDays)-day)
 		}
 	}
+	//当前小时不在规则中
+	if !existsInArray(t.Hour, hour) {
+		nextHour := getMinValueInArray(t.Hour, hour)
+		if nextHour > hour {
+			nowCopy.Add(time.Duration(nextHour-hour) * time.Second * 3600)
+		} else {
+			nowCopy.Add(time.Duration(nextHour+24-hour) * time.Second * 3600)
+		}
+	}
+	//当前分钟不在规则中
+	if !existsInArray(t.Minute, minute) {
+		nextMinute := getMinValueInArray(t.Minute, minute)
+		if nextMinute > minute {
+			nowCopy.Add(time.Duration(nextMinute-minute) * time.Second * 60)
+		} else {
+			nowCopy.Add(time.Duration(nextMinute+60-minute) * time.Second * 60)
+		}
+	}
+	return nowCopy.Format("2006-01-02 15:04:05")
 }
--- test/test.go
+++ test/test.go
@@ -1,7 +1,21 @@
 package main
 
-import "gcron"
+//var b bool
 
 func main() {
-	gcron.EtcdInstance().get
+
+	//b, _ = regexp.MatchString(`^(\d{1,2},)+\d{1,2}/\d{1,2}$`, "1,2,30/3")
+	//fmt.Println(b)
+	//b, _ = regexp.MatchString(`^\d{1,2}-\d{1,2}/\d{1,2}$`, "3-10/3")
+	//fmt.Println(b)
+	//b, _ = regexp.MatchString(`^\*/\d{1,2}$`, "*/10")
+	//fmt.Println(b)
+	//b, _ = regexp.MatchString(`^(\d{1,2},)+\d{1,2}$`, "1,2,3,30")
+	//fmt.Println(b)
+	//b, _ = regexp.MatchString(`^\d{1,2}-\d{1,2}$`, "1-12")
+	//fmt.Println(b)
+	//b, _ = regexp.MatchString(`^\d{1,2}$`, "10")
+	//fmt.Println(b)
+	//b, _ = regexp.MatchString(`^\*$`, "*")
+	//fmt.Println(b)
 }
